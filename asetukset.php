7c23ce7eca9469c1cc8d2200312b1d770af7cfeb
Riku Kilpimaa <RikuKilpimaa@hotmail.fi>
2024-09-18 11:47:03 +0300

    login kasittelija ja profiili

--- asetukset.php
+++ asetukset.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 $PALVELIN = $_SERVER['HTTP_HOST'];
-$PALVELU = "projektit_PHP/php_sovellusmalli";
+$PALVELU = "PuutarhaliikeProjekti";
 $LINKKI_RESETPASSWORD = "resetpassword.php";
 $LINKKI_VERIFICATION = "verification.php";
 $PALVELUOSOITE = "asiakaspalvelu@neilikka.fi";
@@ -11,7 +11,8 @@ define("PROFIILIKUVAKANSIO","profiilikuvat");
 define("PROFIILIKUVAKOKO",5242880);
 define("EMAIL_FROM","wohjelmointi@gmail.com");
 define("EMAIL_FROM_NAME","Ohjelmointikurssi");
-define("YRITYSKERRAT",3);
+
+//HOST PASS USER MYSQL_...
 
 $DB = "neilikka";
 $LOCAL = in_array($_SERVER['REMOTE_ADDR'],array('127.0.0.1','REMOTE_ADDR' => '::1'));
@@ -29,6 +30,7 @@ if ($LOCAL) {
     $EMAIL_ADMIN = $admin_mail;
     }
 elseif (strpos($_SERVER['HTTP_HOST'],"azurewebsites") !== false){
+    define("DEBUG",false);
     $db_server = $_ENV['MYSQL_HOSTNAME'] ?? getenv('MYSQL_HOSTNAME');
     $db_username = $_ENV['MYSQL_USERNAME'] ?? getenv('MYSQL_USERNAME');
     $db_password = $_ENV['MYSQL_PASSWORD'] ?? getenv('MYSQL_PASSWORD');
--- header.php
+++ header.php
@@ -18,6 +18,7 @@ ini_set('default_charset', 'utf-8');
 
 <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 <?php if (isset($js)) echo "<script defer src='$js.js'></script> ";?>
+<script defer src="scripts.js"></script>
     
 </head>
 <body>    
